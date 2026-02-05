4b78071d3e281629183196b0d1a02ec8344dfe0c
syartaaaa <112899861+syartasadiku@users.noreply.github.com>
2026-02-05 22:21:17 +0100

    Adding entry.js

---
+++ server/models/.admin.js/entry.js
@@ -0,0 +1,25 @@
+AdminJS.UserComponents = {}
+import Dashboard from '../components/dashboard'
+AdminJS.UserComponents.Dashboard = Dashboard
+import Login from '../components/login'
+AdminJS.UserComponents.Login = Login
+import SidebarBranding from '../components/sidebarBranding'
+AdminJS.UserComponents.SidebarBranding = SidebarBranding
+import SidebarFooter from '../components/sidebarFooter'
+AdminJS.UserComponents.SidebarFooter = SidebarFooter
+import LinkComponent from '../components/linkComponent'
+AdminJS.UserComponents.LinkComponent = LinkComponent
+import RerouteLinks from '../components/rerouteLinks'
+AdminJS.UserComponents.RerouteLinks = RerouteLinks
+import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
+AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
+import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
+AdminJS.UserComponents.UploadListComponent = UploadListComponent
+import UploadShowComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadShowComponent'
+AdminJS.UserComponents.UploadShowComponent = UploadShowComponent
+import PasswordEditComponent from '../node_modules/@adminjs/passwords/build/components/PasswordEditComponent'
+AdminJS.UserComponents.PasswordEditComponent = PasswordEditComponent
+import ImportComponent from '../node_modules/@adminjs/import-export/lib/components/ImportComponent'
+AdminJS.UserComponents.ImportComponent = ImportComponent
+import ExportComponent from '../node_modules/@adminjs/import-export/lib/components/ExportComponent'
+AdminJS.UserComponents.ExportComponent = ExportComponent
